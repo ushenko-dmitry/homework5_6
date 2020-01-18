b8ad174439c27432dcd20c0a486c92053118e330
DmitryVB <dimaushenko@mail.ru>
2020-01-18 18:45:12 +0300

    complite task

--- src/main/java/ru/mail/dimaushenko/constants/PropertyTask.java
+++ src/main/java/ru/mail/dimaushenko/constants/PropertyTask.java
@@ -2,14 +2,14 @@ package ru.mail.dimaushenko.constants;
 
 public class PropertyTask {
 
-    public static final String C_TASK_AMOUNT_USER_GROUPS = "thirdtask.amount.user_groups";
+    public static final String C_TASK_AMOUNT_USER_GROUPS = "c.amount.user_groups";
 
-    public static final String D_TASK_AMOUNT_USERS = "fourtask.amount.users";
-    public static final String D_TASK_MIN_AGE = "fourtask.min.age";
-    public static final String D_TASK_MAX_AGE = "fourtask.max.age";
+    public static final String D_TASK_AMOUNT_USERS = "d.amount.users";
+    public static final String D_TASK_MIN_AGE = "d.min.age";
+    public static final String D_TASK_MAX_AGE = "d.max.age";
 
-    public static final String G_TASK_AGE_FOR_REMOVE = "seventask.age";
+    public static final String G_TASK_AGE_FOR_REMOVE = "g.age";
 
-    public static final String H_TASK_MIN_AGE = "eighttask.min.age";
-    public static final String H_TASK_MAX_AGE = "eighttask.max.age";
+    public static final String H_TASK_MIN_AGE = "h.min.age";
+    public static final String H_TASK_MAX_AGE = "h.max.age";
 }
