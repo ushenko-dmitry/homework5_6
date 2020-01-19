c69cfa3c35f3c7c4ecbf1f6b7e286ad324c3fac1
DmitryVB <dimaushenko@mail.ru>
2020-01-19 19:19:02 +0300

    move a,b logic in services

--- src/main/java/ru/mail/dimaushenko/controller/impl/BTask.java
+++ src/main/java/ru/mail/dimaushenko/controller/impl/BTask.java
@@ -1,20 +1,10 @@
 package ru.mail.dimaushenko.controller.impl;
 
-import java.lang.invoke.MethodHandles;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import ru.mail.dimaushenko.controller.ControllerService;
-import ru.mail.dimaushenko.utils.PropertyUtil;
-import ru.mail.dimaushenko.utils.impl.PropertyUtilConstantsImpl;
-import ru.mail.dimaushenko.repository.impl.ConnectionPoolImpl;
-
-import static ru.mail.dimaushenko.constants.PropertyConstants.SQL_REQUEST_CREATE_TABLE_USER;
-import static ru.mail.dimaushenko.constants.PropertyConstants.SQL_REQUEST_CREATE_TABLE_USER_GROUP;
-import static ru.mail.dimaushenko.constants.PropertyConstants.SQL_REQUEST_CREATE_TABLE_USER_INFORMATION;
+import ru.mail.dimaushenko.service.TableService;
+import ru.mail.dimaushenko.service.impl.UserGroupTableServiceImpl;
+import ru.mail.dimaushenko.service.impl.UserInformationTableServiceImpl;
+import ru.mail.dimaushenko.service.impl.UserTableServiceImpl;
 
 public class BTask implements ControllerService {
 
@@ -30,24 +20,21 @@ public class BTask implements ControllerService {
         return instance;
     }
 
-    private final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
-    private final PropertyUtil propertyUtil = PropertyUtilConstantsImpl.getInstance();
+    private final TableService userTableService = UserTableServiceImpl.getInstance();
+    private final TableService userGroupTableService = UserGroupTableServiceImpl.getInstance();
+    private final TableService userInformationTableService = UserInformationTableServiceImpl.getInstance();
 
     @Override
     public void run() {
-        try (Connection connection = ConnectionPoolImpl.getInstance().getConnection()) {
-            try (PreparedStatement statement = connection.prepareCall(propertyUtil.getProperty(SQL_REQUEST_CREATE_TABLE_USER_GROUP))) {
-                statement.execute();
+
+        boolean isUserGroupTableCreated = userGroupTableService.createTable();
+        if (isUserGroupTableCreated) {
+            boolean isUserTableCreated = userTableService.createTable();
+            if (isUserTableCreated) {
+                userInformationTableService.createTable();
             }
-            try (PreparedStatement statement = connection.prepareCall(propertyUtil.getProperty(SQL_REQUEST_CREATE_TABLE_USER))) {
-                statement.execute();
-            }
-            try (PreparedStatement statement = connection.prepareCall(propertyUtil.getProperty(SQL_REQUEST_CREATE_TABLE_USER_INFORMATION))) {
-                statement.execute();
-            }
-        } catch (SQLException ex) {
-            LOGGER.error(ex.getMessage(), ex);
         }
+
     }
 
 }
